8595bb6a70d6605fb3c90bf54104343171c95270
Ansarissab <zahidensari116@gmail.com>
2022-10-13 09:45:33 +0500

    Added user model validations

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem "jbuilder"
 gem "redis", "~> 4.0"
 
 gem "slim-rails"
+gem 'phonelib', '~> 0.7.3'
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+  before_save :normalize_phone
+
+  validates :firstname, presence: true, length: { minimum: 3,
+                                                   maximum: 25,
+                                                   message: 'must contain atleast 3 characters!' }
+
+  validates :lastname, presence: true, length: { minimum: 3,
+                                                   maximum: 5,
+                                                   message: 'must contain atleast 3 characters!' }
+
+  validates :phone, phone: true, allow_blank: false
+  validates :email, format: { with: Constants::EMAIL_REGEX, message: "Email invalid"  },
+                              uniqueness: { case_sensitive: false },
+                              length: { minimum: 4, maximum: 254 }
+
+  private
+
+  def normalize_phone
+    self.phone = Phonelib.parse(phone).full_e164.presence
+  end
 end
